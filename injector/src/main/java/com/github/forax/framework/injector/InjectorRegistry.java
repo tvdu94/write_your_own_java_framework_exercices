baeb778d91a9387820e07c3b31d8a746ca4ea296
tvdu94 <thomas.vexiau@gmail.com>
2022-10-04 10:04:50 +0200

    TP2

--- injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
+++ injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
@@ -1,5 +1,46 @@
 package com.github.forax.framework.injector;
 
+import java.beans.PropertyDescriptor;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+
 public final class InjectorRegistry {
-  // TODO
+    private final Map<Class,Supplier> mapOfInjector = new HashMap<>();
+
+    public <T> void registerInstance(Class<T> type, T o){
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(o);
+
+        if (mapOfInjector.putIfAbsent(type,() -> o) != null){
+            throw new IllegalStateException("Already an instance for this type");
+        }
+    }
+
+
+    public <T> T lookupInstance(Class<T> type){
+        T obj = type.cast(mapOfInjector.get(type).get());
+        if (obj == null){
+            throw new NullPointerException("No instance registered for this type");
+        }
+        return obj;
+    }
+
+    public <T> void registerProvider(Class<T> type, Supplier<T> supplier){
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(supplier);
+
+        if (mapOfInjector.putIfAbsent(type,supplier) != null){
+            throw new IllegalArgumentException("Already an supplier for this type");
+        }
+    }
+
+
+    public List<PropertyDescriptor> findInjectableProperties(Class type){
+
+    }
+
 }
